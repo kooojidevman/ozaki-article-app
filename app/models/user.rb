c0e5529bc458320521e7c11acbc074e46a113c4f
kooojidevman <tkkjbiz@gmail.com>
2024-07-21 19:57:26 +0900

    emailのカスタムバリデーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'validator/_email_validator'
+
 # == Schema Information
 #
 # Table name: users
@@ -12,6 +14,8 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  before_validation :downcase_email
+
   # bcrypt:
   # + passwordの暗号化
   # + password_digest -> passwordとして扱える
@@ -23,7 +27,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true, length: { maximum: 30, allow_blank: true }
-  validates :email, presence: true, length: { maximum: 255, allow_blank: true }
+  validates :email, presence: true, email: { allow_blank: true }
   # passwordのバリデーション:
   # + 先頭の文字が「a-zA-Z_-」になっている(\A[\w\-])
   # + 必ず1文字以上繰り返す(+)
@@ -31,4 +35,23 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 8 }, format: { with: /\A[\w\-]+\z/, allow_blank: true }, allow_nil: true
   validates :is_activated, inclusion: { in: [true, false] }
   validates :is_admin, inclusion: { in: [true, false] }
+
+  def email_activated?
+    users = User.where.not(id:)
+    users.find_activated(email).present?
+  end
+
+  class << self
+    # メール認証が既に済んでいるユーザーをemailで検索して返す
+    def find_activated(email)
+      find_by(email:, is_activated: true)
+    end
+  end
+
+  private
+
+  # emailを小文字にする
+  def downcase_email
+    self.email.downcase! if email
+  end
 end
