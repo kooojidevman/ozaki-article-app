7aa964a6711257c159c8a6120ca1f52180d13535
kooojidevman <tkkjbiz@gmail.com>
2024-07-21 12:41:49 +0900

    has_secure_password (bcrypt) 定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,13 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  # bcrypt:
+  # + passwordの暗号化
+  # + password_digest -> passwordとして扱える
+  # + password_confirmation(仮想属性) -> パスワードの一致確認
+  # + 一致バリデーションが自動追加
+  # + 最大72文字のバリデーションが自動追加
+  # + create時の必須バリデーション自動追加
+  # + authenticate(文字列) <- パスワードが一致しているか確認できる
+  has_secure_password
 end
