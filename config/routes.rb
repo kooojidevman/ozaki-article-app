1079e33dea313decdf1d4e595171e63aac02e352
kooojidevman <tkkjbiz@gmail.com>
2024-11-02 14:43:37 +0900

    generate AuthController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post 'auth/register', to: 'auth#register'
+      post 'auth/login', to: 'auth#login'
+
       resources :todos
     end
   end
